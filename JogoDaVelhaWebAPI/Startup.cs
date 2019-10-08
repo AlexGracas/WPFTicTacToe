3fbdec64d92d368f9a01b4cfcf9fc2c409075a27
Alex Pinheiro das Graças <alex.pgracas@gmail.com>
2019-10-07 22:31:43 -0300

    Jogo da Velha Funcional.

--- JogoDaVelhaWebAPI/Startup.cs
+++ JogoDaVelhaWebAPI/Startup.cs
@@ -33,7 +33,7 @@ namespace JogoDaVelhaWebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
